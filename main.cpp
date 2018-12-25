83687a166d3ae63447c95229ae93a5f80a251c48
Noterpopo <popo54336@outlook.com>
2018-12-25 10:02:47 +0800

    finished

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+// AlgHw6.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
@@ -5,227 +7,220 @@
 #include <cmath>
 #include <queue>
 #include <stack>
+#include <vector>
 #include <regex>
+#include <map>
 
 #define MAX_W 1000
+#define MAX_PN 2000000
 
 using namespace std;
+class Edge
+{
+public:
+	int sp;
+	int ep;
+	int max_weighe;
+	int cur_weight;
+	Edge() {}
+	Edge(int s, int e, int mw,int cw)
+	{
+		sp = s;
+		ep = e;
+		max_weighe = mw;
+		cur_weight=cw;
+	}
+};
+vector<Edge> edges;
+vector<int> G[MAX_PN];
+int a[MAX_PN];
+int p[MAX_PN];
+
+void init(int PN)
+{
+    for(int i=0;i<PN;++i)
+    {
+        G[i].clear();
+    }
+    edges.clear();
+}
+void addEdge(int sp,int ep,int we)
+{
+    edges.push_back(Edge(sp,ep,we,0));
+    edges.push_back(Edge(ep,sp,0,0));
+    G[sp].push_back(edges.size()-2);
+    G[ep].push_back(edges.size()-1);
+}
+
+int findMaxFlow(int sp,int ep)
+{
+    int flow=0;
+    while (true)
+    {
+        memset(a,0, sizeof(a));
+        queue<int> Q;
+        Q.push(sp);
+        a[sp]=MAX_W;
+        while(!Q.empty())
+        {
+            int x=Q.front();
+            Q.pop();
+            for(int i=0;i<G[x].size();++i)
+            {
+                Edge& e=edges[G[x][i]];
+                if(!a[e.ep]&&e.max_weighe>e.cur_weight)
+                {
+                    p[e.ep]=G[x][i];
+                    a[e.ep]=min(a[x],e.max_weighe-e.cur_weight);
+                    Q.push(e.ep);
+                }
+            }
+            if(a[ep])
+                break;
+        }
+        if(!a[ep])
+            break;
+        for(int i=ep;i!=sp;i=edges[p[i]].sp)
+        {
+            edges[p[i]].cur_weight+=a[ep];
+            edges[p[i]^1].cur_weight-=a[ep];
+        }
+        flow+=a[ep];
+    }
+    return flow;
+}
 
 int maxFlow = 0;
 
-int **C, **F;
-struct Node {
-    int inedx;
-    Node* parent;
-};
-struct Edge {
-    int sp;
-    int ep;
-};
-void initC(int &PN,int &EN,int type);
-Node* bfs(int N);
-void updateCF(int N, Node* result);
+
+void initC(int &PN, int &EN, int type);
 
 int main() {
-    int num=0,type=0;
-    int PN,EN;
-    cin>>num;
-    for(int q=1;q<=num;++q)
-    {
-        maxFlow=0;
-        cin>>type;
-        srand((unsigned)time(NULL));
-        initC(PN,EN,type);
-        clock_t start, finish;
+	int num = 0, type = 0;
+	int PN, EN;
+	cin >> num;
+	for (int q = 1; q <= num; ++q)
+	{
+		maxFlow = 0;
+		cin >> type;
+		srand((unsigned)time(NULL));
+		initC(PN, EN, type);
+		time_t start, finish;
 
-        //randomInput(C, N);
+		//randomInput(C, N);
 
-        start = clock();
-        while (1) {
-            Node* nodeList = bfs(PN);
-            if (nodeList == NULL)
-                break;
-            updateCF(PN, nodeList);
-        }
-        finish = clock();
+		time(&start);
+		maxFlow=findMaxFlow(0,PN-1);
+		time(&finish);
 
-        double duration = double(finish - start) / CLOCKS_PER_SEC;
-        cout<<"Case "<<q<<": "<<maxFlow<<endl;
-        cout << "需要时间为:" << duration << "s" << endl;
-        for (int i=0; i<PN; ++i)
-        {
-            delete [] C[i];
-            delete [] F[i];
-        }
-        delete [] C;
-        delete [] F;
+		double duration = double(finish - start) / CLOCKS_PER_SEC;
+		cout << "Case " << q << ": " << maxFlow << endl;
+		cout << "需要时间为:" << duration << "s" << endl;
 
-    }
-    return 0;
+	}
+	return 0;
 }
 
-Node* bfs(int N) {
-    Node* nodeList = new Node[N];
-    int* visited = new int[N];
-    for (int i=0; i<N; ++i) {
-        nodeList[i].inedx = i;
-        nodeList[i].parent = NULL;
-        visited[i] = 0;
-    }
-    queue<Node> que;
-    que.push(nodeList[0]);
-    visited[0] = 1;
-    while(!que.empty()) {
-        Node node = que.front();
-        int idx = node.inedx;
-        que.pop();
-        for (int j=1; j<N; j++) {
-            if (C[idx][j] == 0) continue;
-            if (visited[j]) continue;
-            visited[j] = 1;
-            nodeList[j].parent = &nodeList[idx];
-            que.push(nodeList[j]);
-            if (j == N-1) {
-                delete [] visited;
-                return nodeList;
-            }
-        }
-    }
-    delete [] nodeList;
-    delete [] visited;
-    return NULL;
-}
-
-void updateCF(int N, Node* result) {
-    stack<Edge> path;
-    int cfp = MAX_W;
-    int idx = N-1;
-    while(result[idx].parent) {
-        Edge eg;
-        eg.ep = idx;
-        eg.sp = result[idx].parent->inedx;
-        if (C[eg.sp][eg.ep] < cfp)
-            cfp = C[eg.sp][eg.ep];
-        path.push(eg);
-        idx = result[idx].parent->inedx;
-    }
-    while (!path.empty()) {
-        Edge eg = path.top();
-        F[eg.sp][eg.ep] += cfp;
-        C[eg.sp][eg.ep] -= cfp;
-        C[eg.ep][eg.sp] += cfp;
-        path.pop();
-    }
-    maxFlow += cfp;
-    delete [] result;
-
-}
-
-void initC(int &PN,int &EN,int type)
+void initC(int &PN, int &EN, int type)
 {
-    regex numR("[0-9]+");
-    if(type==1||type==2)
-    {
-        cin >> PN>>EN;
-    } else
-    {
-        ifstream infile;
-        infile.open("C:\\Users\\popo\\CLionProjects\\alg-hw6\\roadNet-CA.txt");
-        if ( !infile )
-        {
-            cout <<  "Error opening "<< endl;
-            exit(- 1 );
-        }
-        string line="";
-        getline(infile,line);
-        bool flag= true;
-        for(sregex_token_iterator it(line.begin(),line.end(),numR),end;it != end;it++){
-            if(flag)
-            {
-                flag=!flag;
-                PN=atoi(it->str().c_str());
-            }
-            else
-            {
-                flag=!flag;
-                EN=atoi(it->str().c_str());
-            }
-        }
-        cout<<PN<<EN<<endl;
+	regex numR("[0-9]+");
+	if (type == 1 || type == 2)
+	{
+		cin >> PN >> EN;
+	}
+	else
+	{
+		ifstream infile;
+		infile.open("C:\\Users\\popo\\CLionProjects\\alg-hw6\\roadNet-CA.txt");
+		if (!infile)
+		{
+			cout << "Error opening " << endl;
+			exit(-1);
+		}
+		string line = "";
+		getline(infile, line);
+		bool flag = true;
+		for (sregex_token_iterator it(line.begin(), line.end(), numR), end; it != end; it++) {
+			if (flag)
+			{
+				flag = !flag;
+				PN = atoi(it->str().c_str());
+			}
+			else
+			{
+				flag = !flag;
+				EN = atoi(it->str().c_str());
+			}
+		}
+		cout << PN << " " << EN << endl;
 
-        infile.close();
-    }
+		infile.close();
+	}
 
-    C = new int*[PN];
-    F = new int*[PN];
-    for (int i=0; i<PN; ++i)
-    {
-        C[i] = new int[PN];
-        F[i] = new int[PN];
-    }
-    for (int i=0; i<PN; ++i)
-    {
-        for (int j=0; j<PN; ++j)
-        {
-            C[i][j] = 0;
-            F[i][j] = 0;
-        }
-    }
-    switch (type)
-    {
-        case 1:
-        {
-            for(int i=0;i<EN;++i)
-            {
-                int sp, ep, w;
-                cin >> sp >> ep >> w;
-                C[sp-1][ep-1] += w;
-            }
-            break;
-        }
-        case 2:
-        {
-            for(int i=0;i<EN;++i)
-            {
-                int sp,ep,w;
-                sp=rand()%PN;
-                ep=rand()%PN;
-                if(sp==ep)
-                {
-                    i--;
-                    continue;
-                }
-                w=(rand()%(MAX_W-1))+1;
-                C[sp][ep]+=w;
-            }
-            break;
-        }
-        case 3:
-        {
-            ifstream infile;
-            infile.open("C:\\Users\\popo\\CLionProjects\\alg-hw6\\roadNet-CA.txt");
-            string line="";
-            while (getline(infile,line))
-            {
-                bool flag= true;
-                for(sregex_token_iterator it(line.begin(),line.end(),numR),end;it != end;it++){
-                    if(flag)
-                    {
-                        flag=!flag;
+	init(PN);
+	switch (type)
+	{
+	case 1:
+	{
+		for (int i = 0; i < EN; ++i)
+		{
+			int sp, ep, w;
+			cin >> sp >> ep >> w;
+			addEdge(sp-1,ep-1,w);
+		}
+		break;
+	}
+	case 2:
+	{
+		for (int i = 0; i < EN; ++i)
+		{
+			int sp, ep, w;
+			sp = rand() % PN;
+			ep = rand() % PN;
+			if (sp == ep)
+			{
+				i--;
+				continue;
+			}
+			w = (rand() % (MAX_W - 1)) + 1;
+            addEdge(sp,ep,w);
+		}
+		break;
+	}
+	case 3:
+	{
+		ifstream infile;
+		infile.open("C:\\Users\\popo\\CLionProjects\\alg-hw6\\roadNet-CA.txt");
+		string line = "";
+		int count = 0;
+		time_t start, finish;
+		time(&start);
+		while (getline(infile, line))
+		{
+			int sp, ep;
+			infile >> sp >> ep;
+            addEdge(sp,ep,1);
 
-                    }
-                    else
-                    {
-                        flag=!flag;
+		}
+		time(&finish);
+		cout << finish - start << endl;
+		infile.close();
+		break;
+	}
+	default:
+		break;
 
-                    }
-                }
-            }
-            infile.close();
-            break;
-        }
-        default:
-            break;
-
-    }
+	}
 }
+
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
